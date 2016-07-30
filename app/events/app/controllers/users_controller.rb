783c078c87e9d379226b0e7bcb120daefa11f117
Gustavo Vera Scuderi <Gustavo Vera Scuderi>
2016-07-29 21:15:12 -0400

    Validaciones y fixes

--- app/events/app/controllers/users_controller.rb
+++ app/events/app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
             session[:user_id] = user.id
             redirect_to '/'
         else
-            redirect_to :register
+            render action: :new
         end
     end
 
